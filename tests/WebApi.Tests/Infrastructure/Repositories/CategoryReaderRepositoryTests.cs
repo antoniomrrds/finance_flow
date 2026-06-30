9e737f7b88ad3036b62ace07ec1b359ffe61baac
アントニオ <arqueiroverd@gmail.com>
2026-06-30 11:10:58 -0300

    test(category_reader_repository): ensure that if the name already exists it returns true

--- tests/WebApi.Tests/Infrastructure/Repositories/CategoryReaderRepositoryTests.cs
+++ tests/WebApi.Tests/Infrastructure/Repositories/CategoryReaderRepositoryTests.cs
@@ -11,20 +11,38 @@ public class CategoryReaderRepositoryTests
 {
     private readonly CategoryReaderRepository _sut;
     private readonly Category _category;
+    private readonly AppDbContext _db;
 
     public CategoryReaderRepositoryTests()
     {
         _category = CategoryFixture.GetCategory(true);
-        AppDbContext db = TestDbFactory.Create();
-        _sut = new CategoryReaderRepository(db);
+        _db = TestDbFactory.Create();
+        _sut = new CategoryReaderRepository(_db);
+    }
+
+    private async Task AddCategory()
+    {
+        await _db.Categories.AddAsync(_category);
+        await _db.SaveChangesAsync();
     }
 
     [Fact]
     public async Task HasCategoryWithNameAsync_WhenTheNameDoesNotExist_ShouldReturnFalse()
     {
         //Arrange & Act
-        bool nameExist = await _sut.HasCategoryWithNameAsync(_category.Name);
+        bool nameExists = await _sut.HasCategoryWithNameAsync(_category.Name);
         // Assert
-        nameExist.ShouldBeFalse();
+        nameExists.ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task HasCategoryWithNameAsync_WhenNameAlreadyExists_ShouldReturnTrue()
+    {
+        // Arrange
+        await AddCategory();
+        // Act
+        bool nameExists = await _sut.HasCategoryWithNameAsync(_category.Name);
+        // Assert
+        nameExists.ShouldBeTrue();
     }
 }
